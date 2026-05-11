5e960a9f5e9a40d687f511c438781b5140ccf1ad
Souvik Patra <souvikkpatra06@gmail.com>
2026-05-12 02:04:21 +0530

    Update FactorialCache.c

--- FactorialCache.c
+++ FactorialCache.c
@@ -1,108 +1,58 @@
 import java.util.Scanner;
 
-class FactorialCache {
-    private long[] mFactorials;
+class Factorial {
 
-    public FactorialCache() {
-        this(21);
-    }
+    static long[] fact = new long[21];
 
-    public FactorialCache(int iSize) {
-        if (iSize <= 0) {
-            iSize = 21;
+    public static long factorial(int x) {
+
+        if (x < 0) {
+            throw new IllegalArgumentException(
+                "value of x must be positive"
+            );
         }
-        mFactorials = new long[iSize];
-        buildCache();
-    }
 
-    public void setFactorials(long[] lFactorials) {
-        if (lFactorials != null && lFactorials.length > 0) {
-            mFactorials = lFactorials;
-            buildCache();
+        if (x >= fact.length) {
+            throw new IllegalArgumentException(
+                "result will overflow"
+            );
         }
-    }
 
-    public long[] getFactorials() {
-        return mFactorials;
-    }
+        fact[0] = 1;
 
-    public int getSize() {
-        return mFactorials.length;
-    }
-
-    public long getFactorial(int iX) {
-        if (iX < 0) {
-            throw new IllegalArgumentException("value of x must be positive");
+        for (int i = 1; i <= x; i++) {
+            fact[i] = fact[i - 1] * i;
         }
-        if (iX >= mFactorials.length) {
-            throw new IllegalArgumentException("result will overflow");
-        }
-        return mFactorials[iX];
-    }
 
-    private void buildCache() {
-        mFactorials[0] = 1L;
-        for (int i = 1; i < mFactorials.length; i++) {
-            mFactorials[i] = mFactorials[i - 1] * i;
-        }
+        return fact[x];
     }
 }
 
-interface IFactorialService {
-    public long getFactorial(int iX);
-    public int getMaxSupportedX();
-}
+public class Main {
 
-class FactorialService implements IFactorialService {
-    private FactorialCache mCache;
-
-    public FactorialService() {
-        this(new FactorialCache());
-    }
-
-    public FactorialService(FactorialCache oCache) {
-        if (oCache == null) {
-            oCache = new FactorialCache();
-        }
-        mCache = oCache;
-    }
-
-    public void setCache(FactorialCache oCache) {
-        if (oCache != null) {
-            mCache = oCache;
-        }
-    }
-
-    public FactorialCache getCache() {
-        return mCache;
-    }
-
-    @Override
-    public long getFactorial(int iX) {
-        return mCache.getFactorial(iX);
-    }
-
-    @Override
-    public int getMaxSupportedX() {
-        return mCache.getSize() - 1;
-    }
-}
-
-public class Factorial {
     public static void main(String[] args) {
-        Scanner oScanner = new Scanner(System.in);
-        IFactorialService oService = new FactorialService();
 
-        System.out.print("Enter x: ");
-        int iX = oScanner.nextInt();
+        Scanner sc = new Scanner(System.in);
 
         try {
-            long lAns = oService.getFactorial(iX);
-            System.out.println("Factorial(" + iX + ") = " + lAns);
-        } catch (IllegalArgumentException oEx) {
-            System.out.println(oEx.getMessage());
+
+            System.out.print("Enter a number: ");
+            int n = sc.nextInt();
+
+            long result = Factorial.factorial(n);
+
+            System.out.println(
+                "Factorial of " + n + " = " + result
+            );
         }
 
-        oScanner.close();
+        catch (IllegalArgumentException e) {
+
+            System.out.println(
+                "Exception: " + e.getMessage()
+            );
+        }
+
+        sc.close();
     }
 }
